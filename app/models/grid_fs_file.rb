446f7365cb182c63bf8c05f9f9c3642d4a34e889
Jeremy Hastings <jeremy.hastings@mac.com>
2020-05-15 11:12:17 -0700

    Define initialize method

--- app/models/grid_fs_file.rb
+++ app/models/grid_fs_file.rb
@@ -16,4 +16,23 @@ class GridFsFile
     nil
   end
 
+  def initialize(params={})
+    if params[:_id] # Hash came from GridFS
+      @id = params[:_id].to_s
+      @author = params[:metadata].nil? ? nil : params[:metadata][:author]
+      @topic = params[:metadata].nil? ? nil : params[:metadata][:topic]
+    else # Assume hash came from Rails
+      @id = params[:id]
+      @author = params[:author]
+      @topic = params[:topic]
+    end
+    @chunkSize = params[:chunkSize]
+    @uploadDate = params[:uploadDate]
+    @contentType = params[:contentType]
+    @filename = params[:filename]
+    @length = params[:length]
+    @md5 = params[:md5]
+    @contents = params[:contents]
+  end
+
 end

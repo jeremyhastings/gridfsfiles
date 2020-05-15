b3f9a3e0576b15a73c71a21f3130c6efd547f3b6
Jeremy Hastings <jeremy.hastings@mac.com>
2020-05-15 10:38:05 -0700

    Load mongoid in application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Gridfsfiles
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Bootstraps mongoid within applications -- like rails console
+    Mongoid.load!('./config/mongoid.yml')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

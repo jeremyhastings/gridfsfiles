fd468a0fb23639d5ffccd2adcbdd7a6fdb66574b
Jeremy Hastings <jeremy.hastings@mac.com>
2020-05-15 11:23:52 -0700

    Fix to model

--- app/models/grid_fs_file.rb
+++ app/models/grid_fs_file.rb
@@ -2,7 +2,7 @@ class GridFsFile
   include ActiveModel::Model
   attr_accessor :contentType, :filename, :author, :topic
   attr_writer :contents
-  attr_reader :id, uploadDate, :chunkSize, :length, :md5
+  attr_reader :id, :uploadDate, :chunkSize, :length, :md5
 
   def persisted?
     !@id.nil?
